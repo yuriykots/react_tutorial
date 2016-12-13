9e5280a050c9cd1d5358c95772c9831db8b1bcac
yuriykots <setingm@gmail.com>
2016-12-13 11:37:45 -0500

    Node. Passport function.

---
+++ 15-authentification-passport.js
@@ -0,0 +1,36 @@
+const knex = require("knex")
+const passport = require("passport")
+const LocalStrategy = require("passport-local").Strategy
+// that's format for importing Local
+
+//We will start using LocalStrategy
+passport.use(new LocalStrategy(authenticate))
+//we will need to asycronictly check
+function authenticate(email, password, done){
+  db("users")
+    .where("email", email)
+    .first()
+    .then((user) => {
+      if(!user || user.pasword !== password){
+        return done(null, false, {message: "invalid user and password combination"})
+      }
+      done(null, user)
+    }, done)
+}
+
+//creating a hash and storing in session
+passport.serializeUser(function(user, done){
+  done(null, user.id)
+})
+
+//using session cookie to pull up information about user on server side. 
+passport.deserializeUser(function(id, done){
+  db("users")
+   .where("id", id)
+   .first()
+   .then((user) =>{
+     done(null, user)
+   })
+
+
+})
