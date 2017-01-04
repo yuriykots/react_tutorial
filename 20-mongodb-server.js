66a36f85a4b476f5ac518d69e5077cf4c49d117e
yuriykots <setingm@gmail.com>
2017-01-04 15:08:49 -0500

    Adding new user to mongo db

--- 20-mongodb-server.js
+++ 20-mongodb-server.js
@@ -5,4 +5,10 @@ express()
   .get("/", (req,res,next) =>{
     res.send("ok")
   })
+  .get("/create/:first_name/:last_name/:email/:password," (req, res, next) =>{
+    mongo.db.collection("users")
+     .insert(req.params, (err, result) => {
+       res.send(result)
+     })
+  })
   .listen(3000)
